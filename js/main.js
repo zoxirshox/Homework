4b4571791680437ab1952ee6873af742874c71ac
zoxirshox <mrcewa2@gmail.com>
2022-08-05 14:19:40 +0500

    New file

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var passBound = 9571085.3312;
+var percentage = +prompt("Jonibek seni qancha puling bor?")
+
+if (passBound <= percentage) {
+  console.log("Oq yul, Jonibek")
+}
+else {
+  console.log(`Sayohatga chiqish uchun! ${passBound - percentage}so'm yetmayabdi`)
+  console.log("Jonibek, Shahzoddan sorab ko'r balki berar!")
+  console.log("Agar bermasa uyda kino kurib utir")
+}
